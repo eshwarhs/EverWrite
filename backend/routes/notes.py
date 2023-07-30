dfb8526af446fb4078baa2981b79a4a6cafdba28
Eshwar Hebbur Shivakumar <45142059+eshwarhs@users.noreply.github.com>
2023-07-30 13:53:00 -0700

    Implement List Notes API

---
+++ backend/routes/notes.py
@@ -0,0 +1,27 @@
+from flask import request
+
+from services.list_notes import ListNotes
+from services.get_note import GetNote
+from services.create_note import UpsertNote
+from flask_cors import cross_origin
+
+from lib.helpers import model_json
+
+def load(app):
+    @app.route('/<username>/notes', methods=['GET'])
+    @cross_origin()
+    def list_notes(username):
+        model = ListNotes.run(username)
+        return model_json(model)
+
+    @app.route('/<username>/notes/<note_id>', methods=['GET'])
+    @cross_origin()
+    def get_note(username, note_id):
+            model = GetNote.run(note_id)
+            return model_json(model)
+    
+    @app.route('/<username>/notes', methods=['PUT'])
+    @cross_origin()
+    def create_note(username):
+            model = UpsertNote.run(request)
+            return model_json(model)
