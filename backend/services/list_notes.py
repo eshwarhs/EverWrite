d19387a40c1c4f9ca1f90451f2fc1a68267071fd
Eshwar Hebbur Shivakumar <45142059+eshwarhs@users.noreply.github.com>
2023-08-08 16:43:52 -0700

    Add query parameter to distinguish trash and general notes

--- backend/services/list_notes.py
+++ backend/services/list_notes.py
@@ -3,12 +3,13 @@ from lib.MongoJSONEncoder import MongoJSONEncoder
 import json
 
 class ListNotes:
-    def run(username):
+    def run(request, username):
         model = {
             'errors': None,
             'data': None,
             'code': None
         }
+        
         client = Mongo.client()
         db = client['everwrite']
         users_collection = db['users']
@@ -18,7 +19,7 @@ class ListNotes:
         #     model['errors'] = {'msg': 'User not found'}
         #     model['code'] = 404
         # else:
-        user_notes = notes_collection.find({'username': username})
+        user_notes = notes_collection.find({'username': username, 'deleted': True if 'type' in request.args and request.args['type'] == 'trash' else False})
         model['data'] = json.loads(MongoJSONEncoder().encode(list(user_notes)))
         model['code'] = 200
         return model
